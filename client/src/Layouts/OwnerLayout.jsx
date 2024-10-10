56a44da1fc43be19fec853ff9bdec67ea7d66aba
fazil8848 <128672726+fazil8848@users.noreply.github.com>
2024-10-10 19:40:12 +0530

    side bar fix

--- client/src/Layouts/OwnerLayout.jsx
+++ client/src/Layouts/OwnerLayout.jsx
@@ -31,16 +31,17 @@ const OwnerLayout = () => {
       <div className="flex h-screen overflow-hidden">
         <OwnerNav sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-        <div className="flex flex-1 flex-col overflow-y-auto overflow-x-hidden card">
-          <OwnerHeader
-            sidebarOpen={sidebarOpen}
-            setSidebarOpen={setSidebarOpen}
-          />
+        <div className="flex flex-1 flex-col overflow-y-auto overflow-x-auto card">
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-          <div>
+          <div
+            className={`transition-margin duration-300 ${
+              sidebarOpen ? "ml-64" : ""
+            }`}
+          >
             <ToastContainer />
-            <main className="bg-gray-100">
-              <div className=" my-16 max-w-screen-2xl p-4 md:p-6 2xl:px-10">
+            <main className=" w-full h-screen">
+              <div className="mx-auto my-16 max-w-screen-2xl px-1 md:px-3 py-4 md:py-6 2xl:px-10">
                 <Outlet />
               </div>
             </main>
